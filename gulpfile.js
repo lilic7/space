4aeef0cdd9f42a0fca78c53425dd833b7e863c1d
lilic7 <zubcolilian@mail.ru>
2017-03-18 01:46:11 +0200

    add 7-1 style for Sass folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,28 @@ var gulp = require("gulp");
 var gutil = require("gulp-util");
 var concat = require("gulp-concat");
 var sass = require("gulp-sass");
+var rename = require("gulp-rename");
+var postcss = require("gulp-postcss");
+var sourcemaps = require("gulp-sourcemaps");
+var autoprefixer = require("autoprefixer");
 var pug = require("gulp-pug");
+
 var browserSync = require("browser-sync").create();
+
 
 function browser_sync() {
     browserSync.init({
-        proxy: 'hairstyle.app',
+        proxy: 'template-starter.app',
         files: [
             'public/*.html',
+            'public/css/*.css'
         ],
         port: 7000
     });
 }
 
 function watch() {
-    gulp.watch("sass/*.sass", Sass);
+    gulp.watch(["scss/*.scss", "scss/**/*.scss"], Sass);
     gulp.watch("pug/*.pug", renderPUGtoHTML);
     gulp.watch("pug/**/*.pug", renderPUGtoHTML);
 }
@@ -28,9 +35,17 @@ function renderPUGtoHTML() {
         .pipe(browserSync.stream({once: true}));
 }
 
+
+
 function Sass(){
-    return gulp.src("sass/*.sass")
+    return gulp.src(["scss/*.scss", "scss/**/*.scss"])
         .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest("dest/css"))
+        .pipe(sourcemaps.init())
+        .pipe(postcss([autoprefixer()]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest("dest/prefixed"))
+        .pipe(rename("style.css"))
         .pipe(gulp.dest("public/css"))
         .pipe(browserSync.stream());
 }
