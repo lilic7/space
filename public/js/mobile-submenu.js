1cccc9d4fc55eedae8e2554e1f32ff62b764c426
Lilic <zubcolilian@mail.ru>
2017-04-10 18:04:42 +0300

    start about page

--- public/js/mobile-submenu.js
+++ public/js/mobile-submenu.js
@@ -6,9 +6,9 @@ $(document).ready(function () {
     dropdownIcon.click(function () {
         Submenu.toggleClass('open');
         if(Submenu.hasClass('open')){
-            dropdownIcon.css('background', "url('../img/mobile/mobile-expand.png') no-repeat");
-        } else {
             dropdownIcon.css('background', "url('../img/mobile/mobile-collapse.png') no-repeat");
+        } else {
+            dropdownIcon.css('background', "url('../img/mobile/mobile-expand.png') no-repeat");
         }
     });
 })
