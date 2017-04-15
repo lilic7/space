736038d2fc9eeaabf012cf7ce46aa064659d6432
lilic7 <zubcolilian@mail.ru>
2017-04-15 09:53:22 +0300

    working on navbar

--- public/js/mobile-submenu.js
+++ public/js/mobile-submenu.js
@@ -6,9 +6,9 @@ $(document).ready(function () {
     dropdownIcon.click(function () {
         $(this).next().toggleClass('open');
         if(Submenu.hasClass('open')){
-            $(this).css('background', "url('../img/mobile/mobile-collapse.png') no-repeat");
+            $(this).css('background-image', "url('../img/mobile/mobile-collapse.png')");
         } else {
-            $(this).css('background', "url('../img/mobile/mobile-expand.png') no-repeat");
+            $(this).css('background-image', "url('../img/mobile/mobile-expand.png')");
         }
     });
 })
